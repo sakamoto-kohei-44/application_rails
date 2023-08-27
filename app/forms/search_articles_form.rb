8b2c072259cd0f467937db835d1a160a91db9764
sakamoto-kohei-44 <banbengongping111@gmail.com>
2023-08-27 23:53:30 +0900

    fix

--- app/forms/search_articles_form.rb
+++ app/forms/search_articles_form.rb
@@ -2,24 +2,20 @@ class SearchArticlesForm
   include ActiveModel::Model
   include ActiveModel::Attributes
 
-  attribute :category_id, :integer
   attribute :author_id, :integer
+  attribute :category_id, :integer
   attribute :tag_id, :integer
   attribute :title, :string
-  attribute :state, :integer
   attribute :body, :string
+
 
   def search
     relation = Article.distinct
-
-    relation = relation.by_category(category_id) if category_id.present?
-    relation = relation.by_author(author_id) if author_id.present?
-    relation = relation.by_state(state) if state.present?
-    relation = relation.by_tag(tag_id) if tag_id.present?
-
     title_words.each do |word|
       relation = relation.title_contain(word)
     end
+    relation = relation.by_author(author_id) if author_id.present?
+    relation = relation.by_tag(tag_id) if tag_id.present?
     body_words.each do |word|
       relation = relation.body_contain(word)
     end
